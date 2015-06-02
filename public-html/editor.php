e2847ed1a968aaeb1738c49ecfa6ea2853f292fa
Alex Bates <16batesa@gmail.com>
2015-06-02 19:20:48 +0100

    Create editor.php

---
+++ public-html/editor.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sprint - Editor</title>
+		
+		<?php include 'head.php'; ?>
+		
+		<link rel="stylesheet" href="/css/editor.css" type="text/css">
+		<script src="editor.js">
+	</head>
+	
+	<body>
+		<?php include 'navbar.php'; ?>
+	</body>
+</html>
