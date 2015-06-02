3f006b208f24de2a0770963e98776a7547f6ba56
Alex Bates <16batesa@gmail.com>
2015-06-02 19:15:11 +0100

    Update navbar.php

--- public-html/navbar.php
+++ public-html/navbar.php
@@ -2,9 +2,7 @@
 	<div>
 		<a href="/">Sprint</a>
 		
-		<a href="/editor">Editor</a>
 		<a href="/learn">Learn</a>
-		<a href="/">3</a>
-		<a href="/">4</a>
+		<a href="/editor">Editor</a>
 	</div>
 </nav>
