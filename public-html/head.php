2ffef1cdbf6902d255340daf6bad078bc68bed87
Alex Bates <16batesa@gmail.com>
2015-06-02 18:49:04 +0100

    Update head.php

--- public-html/head.php
+++ public-html/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,300italic,400italic,600italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/main.css" type="text/css">
 <link rel="stylesheet" href="/css/nav.css" type="text/css">
 <link rel="stylesheet" href="/css/footer.css" type="text/css">
