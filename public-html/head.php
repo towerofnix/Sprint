00f787f1c54c08168106ab6ecf66168deaee79c7
Alex Bates <16batesa@gmail.com>
2015-06-02 18:19:52 +0100

    Create head.php

---
+++ public-html/head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="/css/main.css" type="text/css">
+<link rel="stylesheet" href="/css/nav.css" type="text/css">
+<link rel="stylesheet" href="/css/footer.css" type="text/css">
