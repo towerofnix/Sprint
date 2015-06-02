a25d505da6b7aab75e0e0043c3cc18c8fd15bea9
Alex Bates <16batesa@gmail.com>
2015-06-02 20:34:33 +0100

    Update editor.js

--- public-html/js/editor.js
+++ public-html/js/editor.js
@@ -1,5 +1,5 @@
 var editor = ace.edit("editor-code");
 editor.getSession().setUseWrapMode(true);
-editor.getSession().setShowPrintMargin(false);
+editor.setShowPrintMargin(false);
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
