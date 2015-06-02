7905031cfd4f4272703d51c91db29cae0a9fb550
Liam (Aqua) <liamdraws4@gmail.com>
2015-06-02 21:06:36 +0000

    auto commit :3

--- public-html/editor.php
+++ public-html/editor.php
@@ -2,17 +2,17 @@
 <html>
 	<head>
 		<title>Sprint - Editor</title>
-		
+
 		<?php include 'head.php'; ?>
-		
+
 		<link rel="stylesheet" href="/css/editor.css" type="text/css">
-		
+
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.1.9/ace.js"></script>
 	</head>
-	
+
 	<body>
 		<?php include 'navbar.php'; ?>
-		
+
 		<main>
 			<div id="editor-toolbar">
 				<span id="editor-file">File</span>
@@ -21,17 +21,14 @@
 				<div id="editor-go">Go</div>
 				<div id="editor-stop">Stop</div>
 				<div id="editor-fullscreen">Fullscreen</div>
+				<div id="sprites-pane">
+					<!-- All the sprites preview images are put here.
+					You can click on one to open it up in the editor. -->
+				</div>
 			</div>
 			<div id="editor-right-pane">
-				<div id="editor-code">function init() {
-    // this runs when the app is started, like "When Green Flag Clicked"
-}
-
-function update() {
-    // this runs every frame
-}</div>
+				<div id="editor-code"></div>
 			</div>
-			
 			<script src="/js/editor.js"></script>
 		</main>
 	</body>
