a398200019026113706b3e176fa291885f736f67
Alex Bates <16batesa@gmail.com>
2015-06-02 18:22:41 +0100

    Update head.php

--- public-html/head.php
+++ public-html/head.php
@@ -1,3 +1,9 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="stylesheet" href="/css/main.css" type="text/css">
 <link rel="stylesheet" href="/css/nav.css" type="text/css">
 <link rel="stylesheet" href="/css/footer.css" type="text/css">
+
+<script src="/js/modernizr.js"></script>
+<script src="/js/main.js"></script>
